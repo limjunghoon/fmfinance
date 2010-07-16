19bb4d40b6cdb1890ba3a8fe45a9cb3dedecacd6
yongbban <yongbban@eebb329f-6a66-ea59-3a61-84978badfd71>
2010-07-16 11:20:00 +0000

    수입과 지출 기본 레이아웃

--- trunk/Sample/Test/FmFinanceLayout/src/com/fletamuto/sptb/FmFinanceLayout.java
+++ trunk/Sample/Test/FmFinanceLayout/src/com/fletamuto/sptb/FmFinanceLayout.java
@@ -1,7 +1,10 @@
 package com.fletamuto.sptb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FmFinanceLayout extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,23 @@ public class FmFinanceLayout extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button runInputExpense = (Button)findViewById(R.id.BtnInputExpense);
+        runInputExpense.setOnClickListener(new Button.OnClickListener() {
+
+			public void onClick(View v) {
+				Intent intent = new Intent(FmFinanceLayout.this, InputExpenseLayout.class);
+				startActivity(intent);
+			}
+        });
+        
+        Button runInputIncome = (Button)findViewById(R.id.BtnInputIncome);
+        runInputIncome.setOnClickListener(new Button.OnClickListener() {
+
+			public void onClick(View v) {
+				Intent intent = new Intent(FmFinanceLayout.this, InputIncomeLayout.class);
+				startActivity(intent);
+			}
+        });
     }
 }
