7fd5183b1d0efe3b091efaf1787bfeb1e04c3b46
yongbban <yongbban@eebb329f-6a66-ea59-3a61-84978badfd71>
2010-07-19 12:55:07 +0000

    목록 리스트 구현

---
+++ trunk/Sample/Test/FmFinanceLayout/src/com/fletamuto/sptb/MainReportLayout.java
@@ -0,0 +1,42 @@
+package com.fletamuto.sptb;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainReportLayout extends ListActivity {
+	
+	private ArrayList<Class<?>> reportList;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      
+        
+        ArrayAdapter<CharSequence> adapter;
+        adapter = ArrayAdapter.createFromResource(this, R.array.report_list, android.R.layout.simple_list_item_1);
+       
+        setListAdapter(adapter);
+    
+        reportList = new ArrayList<Class<?>>();
+        reportList.add(ReportExpenseLayout.class);
+        reportList.add(ReportIncomeLayout.class);
+        reportList.add(ReportAssetsLayout.class);
+        reportList.add(ReportLiabilityLayout.class);
+    }
+    
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+    	
+    	if (reportList.size() <= position) return;
+    	Intent intent = new Intent(this, reportList.get(position));
+		startActivity(intent);
+    }
+
+}
---
+++ trunk/Sample/Test/FmFinanceLayout/src/com/fletamuto/sptb/ReportLiabilityLayout.java
@@ -0,0 +1,26 @@
+package com.fletamuto.sptb;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ReportLiabilityLayout extends ListActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+    }
+    
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+    }
+
+}
