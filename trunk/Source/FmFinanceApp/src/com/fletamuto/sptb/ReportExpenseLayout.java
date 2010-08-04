89d53ef2b506048833f863464e1693a0521a4d32
yongbban <yongbban@eebb329f-6a66-ea59-3a61-84978badfd71>
2010-08-04 12:48:52 +0000

    레포트 클래스 베이스 리포트에 상속받도록 구조 변경

--- trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/ReportExpenseLayout.java
+++ trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/ReportExpenseLayout.java
@@ -1,107 +1,51 @@
 package com.fletamuto.sptb;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
-import android.app.ListActivity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.fletamuto.sptb.data.AssetsItem;
 import com.fletamuto.sptb.data.ExpenseItem;
 import com.fletamuto.sptb.data.FinanceItem;
 import com.fletamuto.sptb.db.DBMgr;
 
-public class ReportExpenseLayout extends ListActivity {
-
-    private ArrayList<FinanceItem> items;
-    ExpenseItemAdapter adapter = null;
+public class ReportExpenseLayout extends ReportBaseLayout {
     
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        if (getItemsFromDB() == false) {
+        if (getItemsFromDB(ExpenseItem.TYPE) == false) {
         	return;
         }
         
-        adapter = new ExpenseItemAdapter(this, R.layout.report_list_expense, items);
-		setListAdapter(adapter); 
-    }
-    
-    protected boolean getItemsFromDB() {
-    	items = DBMgr.getInstance().getAllItems(ExpenseItem.TYPE);
-        if (items == null) {
-        	return false;
-        }
-        return true;
+        setListAdapter(R.layout.report_list_expense);
     }
     
     protected void onListItemClick(ListView l, View v, int position, long id) {
     }
     
-    public class ExpenseItemAdapter extends ArrayAdapter<FinanceItem> {
-    	int resource;
-
-		public ExpenseItemAdapter(Context context, int resource,
-				 List<FinanceItem> objects) {
-			super(context, resource, objects);
-			this.resource = resource;
-		}
+    protected void setListViewText(FinanceItem financeItem, View convertView) {
+    	ExpenseItem item = (ExpenseItem)financeItem;
 		
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent ) {
-			LinearLayout expenseListView;
-			ExpenseItem item = (ExpenseItem)getItem(position);
-			
-			if (convertView == null) {
-				expenseListView = new LinearLayout(getContext());
-				String inflater = Context.LAYOUT_INFLATER_SERVICE;
-				LayoutInflater li;
-				li = (LayoutInflater)getContext().getSystemService(inflater);
-				li.inflate(resource, expenseListView, true);
-			}
-			else {
-				expenseListView = (LinearLayout)convertView;
-			}
-			
-			((TextView)expenseListView.findViewById(R.id.TVExpenseReportListDate)).setText("날짜 : " + item.getDateString());			
-			((TextView)expenseListView.findViewById(R.id.TVExpenseReportListAmount)).setText(String.format("금액 : %,d원", item.getAmount()));
-			((TextView)expenseListView.findViewById(R.id.TVExpenseReportListMemo)).setText("메모 : " + item.getMemo());
-			String categoryText = String.format("%s - %s", item.getCategory().getName(), item.getSubCategory().getName());
-			((TextView)expenseListView.findViewById(R.id.TVExpenseReportListCategory)).setText("분류 : " + categoryText);
-			
-			Button btn = (Button)expenseListView.findViewById(R.id.BtnReportExpenseDelete);
-			btn.setTag(R.id.delete_id, new Integer(item.getId()));
-			btn.setTag(R.id.delete_position, new Integer(position));
-			btn.setOnClickListener(deleteBtn);
-			
-			return expenseListView;
-		}
+		((TextView)convertView.findViewById(R.id.TVExpenseReportListDate)).setText(item.getDateString());			
+		((TextView)convertView.findViewById(R.id.TVExpenseReportListAmount)).setText(String.format("금액 : %,d원", item.getAmount()));
+		((TextView)convertView.findViewById(R.id.TVExpenseReportListMemo)).setText(item.getMemo());
+		String categoryText = String.format("%s - %s", item.getCategory().getName(), item.getSubCategory().getName());
+		((TextView)convertView.findViewById(R.id.TVExpenseReportListCategory)).setText(categoryText);
+	}
+    
+    protected void setDeleteBtnListener(View convertView, int itemId, int position) {
+    	Button btnDelete = (Button)convertView.findViewById(R.id.BtnReportExpenseDelete);
+		btnDelete.setTag(R.id.delete_id, new Integer(itemId));
+		btnDelete.setTag(R.id.delete_position, new Integer(position));
+		btnDelete.setOnClickListener(deleteBtnListener);
     }
     
-    Button.OnClickListener deleteBtn = new  Button.OnClickListener(){
-    	
-		public void onClick(View arg0) {
-			Integer position = (Integer)arg0.getTag(R.id.delete_position);
-			Integer id = (Integer)arg0.getTag(R.id.delete_id);
-			if (DBMgr.getInstance().deleteItem(ExpenseItem.TYPE, id) == 0) {
-				Log.e(LogTag.LAYOUT, "== noting delete id : " + id);
-			}
-			else {
-				items.remove(position.intValue());
-				adapter.notifyDataSetChanged();
-			}
-		}
-    };
+    @Override
+	protected int deleteItemToDB(int id) {
+		return DBMgr.getInstance().deleteItem(ExpenseItem.TYPE, id);
+	}
 }
--- trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/ReportIncomeLayout.java
+++ trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/ReportIncomeLayout.java
@@ -1,19 +1,9 @@
 package com.fletamuto.sptb;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
-import android.app.ListActivity;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,86 +11,41 @@ import com.fletamuto.sptb.data.FinanceItem;
 import com.fletamuto.sptb.data.IncomeItem;
 import com.fletamuto.sptb.db.DBMgr;
 
-public class ReportIncomeLayout extends ListActivity {
-	ArrayList<FinanceItem> items = null;
-	IncomeItemAdapter adapter = null;
-	
+public class ReportIncomeLayout extends ReportBaseLayout {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        if (getItemsFromDB() == false) {
+        if (getItemsFromDB(IncomeItem.TYPE) == false) {
         	return;
         }
-
-        adapter = new IncomeItemAdapter(this, R.layout.report_list_income, items);
-		setListAdapter(adapter); 
+        
+        setListAdapter(R.layout.report_list_income);
     }
     
-    protected boolean getItemsFromDB() {
-    	items = DBMgr.getInstance().getAllItems(IncomeItem.TYPE);
-        if (items == null) {
-        	return false;
-        }
-        return true;
-    }
     
     protected void onListItemClick(ListView l, View v, int position, long id) {
 
     }
     
-    public class IncomeItemAdapter extends ArrayAdapter<FinanceItem> {
-    	int resource;
-
-		public IncomeItemAdapter(Context context, int resource,
-				 List<FinanceItem> objects) {
-			super(context, resource, objects);
-			this.resource = resource;
-		}
-		
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			LinearLayout incomeListView;
-			IncomeItem item = (IncomeItem)getItem(position);
-			
-			if (convertView == null) {
-				incomeListView = new LinearLayout(getContext());
-				String inflater = Context.LAYOUT_INFLATER_SERVICE;
-				LayoutInflater li;
-				li = (LayoutInflater)getContext().getSystemService(inflater);
-				li.inflate(resource, incomeListView, true);
-			}
-			else {
-				incomeListView = (LinearLayout)convertView;
-			}
-			
-			((TextView)incomeListView.findViewById(R.id.TVIncomeReportListDate)).setText("날짜 : " + item.getDateString());			
-			((TextView)incomeListView.findViewById(R.id.TVIncomeReportListAmount)).setText(String.format("금액 : %,d원", item.getAmount()));
-			((TextView)incomeListView.findViewById(R.id.TVIncomeReportListMemo)).setText("메모 : " + item.getMemo());
-			((TextView)incomeListView.findViewById(R.id.TVIncomeReportListCategory)).setText("분류 : " + item.getCategory().getName());
-			
-			Button btn = (Button)incomeListView.findViewById(R.id.BtnReportIncomeDelete);
-			btn.setTag(R.id.delete_id, new Integer(item.getId()));
-			btn.setTag(R.id.delete_position, new Integer(position));
-			btn.setOnClickListener(deleteBtn);
-			
-			return incomeListView;
-		}
+    protected void setListViewText(FinanceItem financeItem, View convertView) {
+    	IncomeItem item = (IncomeItem)financeItem;
+    	((TextView)convertView.findViewById(R.id.TVIncomeReportListDate)).setText("날짜 : " + item.getDateString());			
+		((TextView)convertView.findViewById(R.id.TVIncomeReportListAmount)).setText(String.format("금액 : %,d원", item.getAmount()));
+		((TextView)convertView.findViewById(R.id.TVIncomeReportListMemo)).setText("메모 : " + item.getMemo());
+		((TextView)convertView.findViewById(R.id.TVIncomeReportListCategory)).setText("분류 : " + item.getCategory().getName());
+	}
+    
+    protected void setDeleteBtnListener(View convertView, int itemId, int position) {
+    	Button btnDelete = (Button)convertView.findViewById(R.id.BtnReportIncomeDelete);
+		btnDelete.setTag(R.id.delete_id, new Integer(itemId));
+		btnDelete.setTag(R.id.delete_position, new Integer(position));
+		btnDelete.setOnClickListener(deleteBtnListener);
     }
     
-    Button.OnClickListener deleteBtn = new  Button.OnClickListener(){
-    	
-		public void onClick(View arg0) {
-			Integer position = (Integer)arg0.getTag(R.id.delete_position);
-			Integer id = (Integer)arg0.getTag(R.id.delete_id);
-			if (DBMgr.getInstance().deleteItem(IncomeItem.TYPE, id) == 0) {
-				Log.e(LogTag.LAYOUT, "== noting delete id : " + id);
-			}
-			else {
-				items.remove(position.intValue());
-				adapter.notifyDataSetChanged();
-			}
-		}
-	};
+    @Override
+	protected int deleteItemToDB(int id) {
+		return DBMgr.getInstance().deleteItem(IncomeItem.TYPE, id);
+	}
 }
