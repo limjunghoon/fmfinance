71191081eeefb47473fb17a0ffe8a395f76491d0
yongbban <yongbban@eebb329f-6a66-ea59-3a61-84978badfd71>
2011-01-05 05:14:12 +0000

    주식 히스토리 뷰 수정

--- trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/ReportAssetsHistoryLayout.java
+++ trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/ReportAssetsHistoryLayout.java
@@ -2,6 +2,7 @@ package com.fletamuto.sptb;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,7 +10,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.fletamuto.sptb.data.AssetsItem;
+import com.fletamuto.sptb.data.AssetsStockItem;
 import com.fletamuto.sptb.data.FinanceItem;
+import com.fletamuto.sptb.data.ItemDef;
 import com.fletamuto.sptb.db.DBMgr;
 import com.fletamuto.sptb.util.Revenue;
 
@@ -51,23 +54,44 @@ public class ReportAssetsHistoryLayout extends ReportBaseLayout {
     
     protected void setListViewText(FinanceItem financeItem, View convertView) {
     	AssetsItem item = (AssetsItem)financeItem;
-		
-    	//((TextView)convertView.findViewById(R.id.TVAssetsReportListTitle)).setText("제목 : " + item.getTitle());
-    	((TextView)convertView.findViewById(R.id.TVAssetsReportListTitle)).setVisibility(View.GONE);
-		((TextView)convertView.findViewById(R.id.TVAssetsReportListDate)).setText("날짜 : " + item.getCreateDateString());			
-		((TextView)convertView.findViewById(R.id.TVAssetsReportListAmount)).setText(String.format("금액 : %,d원", item.getAmount()));
-		((TextView)convertView.findViewById(R.id.TVAssetsReportListCategory)).setVisibility(View.GONE);
-		
-		if (item.getMemo() == null) {
-			((TextView)convertView.findViewById(R.id.TVAssetsReportListMemo)).setVisibility(View.GONE);
+    	
+    	if (mItem.getExtendType() == AssetsStockItem.EXEND_TYPE) {
+    		setListViewTextStock(item, convertView);
 		}
-		else {
-			((TextView)convertView.findViewById(R.id.TVAssetsReportListMemo)).setText(String.format("메모 : %s", item.getMemo()));
-		}
+    	else {
+    		//((TextView)convertView.findViewById(R.id.TVAssetsReportListTitle)).setText("제목 : " + item.getTitle());
+        	((TextView)convertView.findViewById(R.id.TVAssetsReportListTitle)).setVisibility(View.GONE);
+    		((TextView)convertView.findViewById(R.id.TVAssetsReportListDate)).setText("날짜 : " + item.getCreateDateString());			
+    		((TextView)convertView.findViewById(R.id.TVAssetsReportListAmount)).setText(String.format("금액 : %,d원", item.getAmount()));
+    		((TextView)convertView.findViewById(R.id.TVAssetsReportListCategory)).setVisibility(View.GONE);
+    		
+    		if (item.getMemo() == null) {
+    			((TextView)convertView.findViewById(R.id.TVAssetsReportListMemo)).setVisibility(View.GONE);
+    		}
+    		else {
+    			((TextView)convertView.findViewById(R.id.TVAssetsReportListMemo)).setText(String.format("메모 : %s", item.getMemo()));
+    		}
+    		
+    		((TextView)convertView.findViewById(R.id.TVAssetsReportListRevenue)).setText(String.format("수익율 : %s", Revenue.getString(mPurchasePrice, item.getAmount())));
+    	}
 		
-		((TextView)convertView.findViewById(R.id.TVAssetsReportListRevenue)).setText(String.format("수익율 : %s", Revenue.getString(mPurchasePrice, item.getAmount())));
-		
+    	
 	}
+    
+    protected void setListViewTextStock(AssetsItem stock, View convertView) {
+    	TextView tvChangeDate = (TextView) convertView.findViewById(R.id.TVAssetsStockChageDate);
+    	tvChangeDate.setVisibility(View.VISIBLE);
+    	tvChangeDate.setText("날짜 : " + stock.getCreateDateString());
+    	TextView tvTitle = ((TextView)convertView.findViewById(R.id.TVAssetsStockTitle)); 
+    	tvTitle.setText((stock.getState() == AssetsStockItem.SELL) ? "매도" : "매수");
+    	tvTitle.setTextColor((stock.getState() == AssetsStockItem.SELL) ? Color.BLUE : Color.RED);
+		((TextView)convertView.findViewById(R.id.TVAssetsStockAmount)).setText(String.format("금액 :  %,d원", stock.getTotalAmount()));
+		((TextView)convertView.findViewById(R.id.TVAssetsStockMeanPrice)).setText(String.format("주당 금액 : %,d원", stock.getAmount()));
+		((TextView)convertView.findViewById(R.id.TVAssetsStockCount)).setText("수량 : " + stock.getCount() + "주");
+		((TextView)convertView.findViewById(R.id.TVAssetsStockMemo)).setText("메모 : " + stock.getMemo());
+		((TextView)convertView.findViewById(R.id.TVAssetsStockCurrentPrice)).setVisibility(View.GONE);
+		((TextView)convertView.findViewById(R.id.TVAssetsStockRevenue)).setVisibility(View.GONE);
+    }
     
     protected void setDeleteBtnListener(View convertView, int itemId, int position) {
     	Button btnDelete = (Button)convertView.findViewById(R.id.BtnReportAssetsDelete);
@@ -92,7 +116,13 @@ public class ReportAssetsHistoryLayout extends ReportBaseLayout {
 
 	@Override
 	protected int getAdapterResource() {
-		return R.layout.report_list_assets;
+		if (mItem.getExtendType() == AssetsStockItem.EXEND_TYPE) {
+			return R.layout.report_list_assets_stock;
+		}
+		else {
+			return R.layout.report_list_assets;
+		}
+		
 	}
 
 	protected void updateListItem() {
