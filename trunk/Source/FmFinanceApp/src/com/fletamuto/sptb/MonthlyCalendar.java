a5a302d73c2affeb0df0c9ddb13c96365faf49e9
withjp01 <withjp01@eebb329f-6a66-ea59-3a61-84978badfd71>
2011-01-28 01:44:20 +0000

    슬라이딩 UI 를 위한 Base Activity 적용

--- trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/MonthlyCalendar.java
+++ trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/MonthlyCalendar.java
@@ -1,46 +1,31 @@
 package com.fletamuto.sptb;
 
+import com.fletamuto.common.control.BaseSlidingActivity;
 import java.util.Calendar;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.WindowManager;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.DecelerateInterpolator;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MonthlyCalendar extends Activity {
+public class MonthlyCalendar extends BaseSlidingActivity {
 	
 	private MonthlyCalendarAdapter mCalendarAdapter;
 	private static final String INTENT_KEY_YYYYMM = "YYYYMM";
-	LinearLayout layout = null;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        //화면을 dimming 하는 부분
-        WindowManager.LayoutParams lpWindow = new WindowManager.LayoutParams();    
-        lpWindow.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-        lpWindow.dimAmount = 0.75f;
-        getWindow().setAttributes(lpWindow);
-        
         setContentView(R.layout.monthly_calendar);
-
-        layout = (LinearLayout) findViewById(R.id.CalendarLL);
         
-        //Animation 으로 달력 보이기
+        //sliding 을 위해 View 설정 하고 animation 시작 함수 호출 하는 부분 start
+        setSlidingView(findViewById(R.id.CalendarLL));
         appearAnimation();
+        //end
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();        
@@ -58,52 +43,6 @@ public class MonthlyCalendar extends Activity {
 		setDateMoveButtonHandler(R.id.nextYear, Calendar.YEAR, 1);
 		setDateMoveButtonHandler(R.id.nextMonth, Calendar.MONTH, 1);
 
-    }
-    
-    /***************************************************/
-    /** 애니메이션 설정 **/
-    /***************************************************/
-    //위로 올라 오는
-    void appearAnimation() {
-     Animation appear = new TranslateAnimation(
-    		 Animation.RELATIVE_TO_PARENT, 0.0f, 
-             Animation.RELATIVE_TO_PARENT, 0.0f,
-             Animation.RELATIVE_TO_PARENT, 1.0f,
-             Animation.RELATIVE_TO_PARENT, 0.0f
-             );
-     appear.setDuration(700);
-     appear.setInterpolator(new AccelerateInterpolator());
-     appear.setFillAfter(true);
-     
-     layout.startAnimation(appear);
-     
-    }
-    
-    //아래로 내려가는
-    void disAppearAnimation() {
-     Animation disappear = new TranslateAnimation(
-    		 Animation.RELATIVE_TO_PARENT, 0.0f,
-    		 Animation.RELATIVE_TO_PARENT, 0.0f,
-    		 Animation.RELATIVE_TO_PARENT, 0.0f,
-    		 Animation.RELATIVE_TO_PARENT, 1.0f
-     );
-     
-     //달력 사라진 후 Activity 죽이기
-     disappear.setAnimationListener(new AnimationListener() {
-    	public void onAnimationEnd(Animation animaiotn) {
-    		finish();
-    	}
-    	
-    	public void onAnimationStart(Animation animaiotn) {;}
-    	public void onAnimationRepeat(Animation animaiotn) {;}
-     });
-     
-     disappear.setDuration(500);
-     disappear.setInterpolator(new DecelerateInterpolator());
-     disappear.setFillAfter(true);
-     
-     layout.startAnimation(disappear);
-     
     }
     
     private void setCalendar(Calendar cal) {
@@ -144,18 +83,5 @@ public class MonthlyCalendar extends Activity {
 			}
 		});
     	
-    }
-    
-    //Back 키 처리
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event){
-    	
-    	if(keyCode==KeyEvent.KEYCODE_BACK){
-    		disAppearAnimation();
-    		setResult(RESULT_CANCELED);
-    	}
-      
-    	return true;
-    }	
-       
+    }       
 }
