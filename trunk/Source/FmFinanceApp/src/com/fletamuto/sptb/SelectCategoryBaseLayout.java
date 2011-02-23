73b90ffe178b8cc0a335b5d4fab3089e7f843898
withjp01 <withjp01@eebb329f-6a66-ea59-3a61-84978badfd71>
2011-02-23 12:07:01 +0000

    분류 태그 회사 관련 이미지 및 관련 상수 추가

--- trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/SelectCategoryBaseLayout.java
+++ trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/SelectCategoryBaseLayout.java
@@ -26,6 +26,7 @@ import com.fletamuto.sptb.data.Category;
 import com.fletamuto.sptb.data.ExpenseItem;
 import com.fletamuto.sptb.db.DBMgr;
 import com.fletamuto.sptb.util.LogTag;
+import com.fletamuto.sptb.ConstantImagesArray;
 
 public abstract class SelectCategoryBaseLayout extends SelectGridBaseLayout {
 	private static boolean mSelectSubCategory = true;
@@ -45,39 +46,6 @@ public abstract class SelectCategoryBaseLayout extends SelectGridBaseLayout {
 	
 	public final static int ACT_SUB_CATEGORY = MsgDef.ActRequest.ACT_SUB_CATEGORY;
 	public final static int ACT_EDIT_CATEGORY = MsgDef.ActRequest.ACT_EDIT_CATEGORY;
-	
-	public final static int[] CATEGORY_IMAGES = {R.drawable.category_001, R.drawable.category_002, R.drawable.category_003,
-		R.drawable.category_004, R.drawable.category_005, R.drawable.category_006, R.drawable.category_007, R.drawable.category_008,
-		R.drawable.category_009, R.drawable.category_010, R.drawable.category_011, R.drawable.category_012, R.drawable.category_013,
-		R.drawable.category_014, R.drawable.category_015, R.drawable.category_016, R.drawable.category_017, R.drawable.category_018,
-		R.drawable.category_019, R.drawable.category_020, R.drawable.category_021, R.drawable.category_022, R.drawable.category_023,
-		R.drawable.category_024, R.drawable.category_025, R.drawable.category_026, R.drawable.category_027, R.drawable.category_028,
-		R.drawable.category_029, R.drawable.category_030, R.drawable.category_031, R.drawable.category_032, R.drawable.category_033,
-		R.drawable.category_034, R.drawable.category_035, R.drawable.category_036, R.drawable.category_037, R.drawable.category_038,
-		R.drawable.category_039, R.drawable.category_040, R.drawable.category_041, R.drawable.category_042, R.drawable.category_043,
-		R.drawable.category_044, R.drawable.category_045, R.drawable.category_046, R.drawable.category_047, R.drawable.category_048,
-		R.drawable.category_049, R.drawable.category_050, R.drawable.category_051, R.drawable.category_052, R.drawable.category_053,
-		R.drawable.category_054, R.drawable.category_055, R.drawable.category_056, R.drawable.category_057, R.drawable.category_058,
-		R.drawable.category_059, R.drawable.category_060, R.drawable.category_061, R.drawable.category_062, R.drawable.category_063,
-		R.drawable.category_064, R.drawable.category_065, R.drawable.category_066, R.drawable.category_067, R.drawable.category_068,
-		R.drawable.category_069, R.drawable.category_070, R.drawable.category_071, R.drawable.category_072, R.drawable.category_073,
-		R.drawable.category_074, R.drawable.category_075, R.drawable.category_076, R.drawable.category_077, R.drawable.category_078,
-		R.drawable.category_079, R.drawable.category_080, R.drawable.category_081, R.drawable.category_082, R.drawable.category_083,
-		R.drawable.category_084, R.drawable.category_085, R.drawable.category_086, R.drawable.category_087, R.drawable.category_088,
-		R.drawable.category_089, R.drawable.category_090, R.drawable.category_091, R.drawable.category_092, R.drawable.category_093,
-		R.drawable.category_094, R.drawable.category_095, R.drawable.category_096, R.drawable.category_097, R.drawable.category_098,
-		R.drawable.category_099, R.drawable.category_100, R.drawable.category_101, R.drawable.category_102, R.drawable.category_103,
-		R.drawable.category_104, R.drawable.category_105, R.drawable.category_106, R.drawable.category_107, R.drawable.category_108,
-		R.drawable.category_109, R.drawable.category_110, R.drawable.category_111, R.drawable.category_112, R.drawable.category_113,
-		R.drawable.category_114, R.drawable.category_115, R.drawable.category_116, R.drawable.category_117, R.drawable.category_118,
-		R.drawable.category_119, R.drawable.category_120, R.drawable.category_121, R.drawable.category_122, R.drawable.category_123,
-		R.drawable.category_124, R.drawable.category_125, R.drawable.category_126, R.drawable.category_127, R.drawable.category_128,
-		R.drawable.category_129, R.drawable.category_130, R.drawable.category_131, R.drawable.category_132, R.drawable.category_133,
-		R.drawable.category_134, R.drawable.category_135, R.drawable.category_136, R.drawable.category_137, R.drawable.category_138,
-		R.drawable.category_139, R.drawable.category_140, R.drawable.category_141, R.drawable.category_142, R.drawable.category_143,
-		R.drawable.category_144, R.drawable.category_145, R.drawable.category_146, R.drawable.category_147, R.drawable.category_148,
-		R.drawable.category_149, R.drawable.category_150};
-	
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -241,9 +209,9 @@ public abstract class SelectCategoryBaseLayout extends SelectGridBaseLayout {
 				if (getEditCategoryMode() && getSubCategoryMode()) {
 //					button.setText(category.getName());
 					if (category.getID() < -1) {
-						button.setImageResource(CATEGORY_IMAGES[149]);
+						button.setImageResource(ConstantImagesArray.CATEGORY_IMAGES[149]);
 					} else {
-						button.setImageResource(CATEGORY_IMAGES[category.getImageIndex()-1]);
+						button.setImageResource(ConstantImagesArray.CATEGORY_IMAGES[category.getImageIndex()-1]);
 					}
 					tv.setText(category.getName());
 					button.setTag(category);
@@ -264,9 +232,9 @@ public abstract class SelectCategoryBaseLayout extends SelectGridBaseLayout {
 //			button.setText(category.getName());
 
 			if (category.getID() < -1) {
-				button.setImageResource(CATEGORY_IMAGES[149]);
+				button.setImageResource(ConstantImagesArray.CATEGORY_IMAGES[149]);
 			} else {
-				button.setImageResource(CATEGORY_IMAGES[category.getImageIndex()-1]);
+				button.setImageResource(ConstantImagesArray.CATEGORY_IMAGES[category.getImageIndex()-1]);
 			}
 			
 			tv.setText(category.getName());
