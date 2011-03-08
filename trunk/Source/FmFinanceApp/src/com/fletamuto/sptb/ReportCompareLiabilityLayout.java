72c7872c2ba568b5e97355c83923931d5ac55c67
yongbban <yongbban@eebb329f-6a66-ea59-3a61-84978badfd71>
2011-03-08 07:50:53 +0000

    통계
    부채 년/월 보기 기능 구현
    부채 분류 및 히스토리 기능 추가
    테그 년/월 보기 기능 구현
    테그 분류보기 기능 추가
    월별 지출 비율 보기 기능 추가

--- trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/ReportCompareLiabilityLayout.java
+++ trunk/Source/FmFinanceApp/src/com/fletamuto/sptb/ReportCompareLiabilityLayout.java
@@ -3,15 +3,22 @@ package com.fletamuto.sptb;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.fletamuto.sptb.data.AssetsItem;
+import com.fletamuto.sptb.data.Category;
 import com.fletamuto.sptb.data.CategoryAmount;
+import com.fletamuto.sptb.data.FinanceItem;
 import com.fletamuto.sptb.data.LiabilityItem;
 import com.fletamuto.sptb.db.DBMgr;
 import com.fletamuto.sptb.view.FmBaseLayout;
 
 public class ReportCompareLiabilityLayout extends ReportBaseCompare {
 	public static final int ACT_ADD_LIABLITY = MsgDef.ActRequest.ACT_ADD_LIABLITY;
+	
+	/** 메인 분류 아이디 -1이면 전체분류*/
+	public Category mMainCategory = null;
 	
 	/** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
@@ -25,37 +32,56 @@ public class ReportCompareLiabilityLayout extends ReportBaseCompare {
     	updateChildView();
     }
 
-	private void setAddButtonListener() {
-		setTitleButtonListener(FmTitleLayout.BTN_RIGTH_01, new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				Intent intent = new Intent(ReportCompareLiabilityLayout.this, SelectCategoryLiabilityLayout.class);
-				startActivityForResult(intent, ACT_ADD_LIABLITY);
-			}
-		});
-	}
+//	private void setAddButtonListener() {
+//		setTitleButtonListener(FmTitleLayout.BTN_RIGTH_01, new View.OnClickListener() {
+//			
+//			public void onClick(View v) {
+//				Intent intent = new Intent(ReportCompareLiabilityLayout.this, SelectCategoryLiabilityLayout.class);
+//				startActivityForResult(intent, ACT_ADD_LIABLITY);
+//			}
+//		});
+//	}
     
     @Override
     protected void initialize() {
     	setItemType(LiabilityItem.TYPE);
+    	
+    	int categoryID = getIntent().getIntExtra(MsgDef.ExtraNames.CATEGORY_ID, -1);
+    	if (categoryID != -1) {
+    		mMainCategory = DBMgr.getCategoryFromID(mType, categoryID);
+    	}
+    	
     	super.initialize();
     }
     
     protected void getData() {
-		mFinanceItems = DBMgr.getAllItems(mType);
-		mTotalAmout = DBMgr.getTotalAmount(mType);
+    	if (mMainCategory == null) {
+    		mFinanceItems = DBMgr.getAllItems(mType);
+    		mTotalAmout = DBMgr.getTotalAmount(mType);
+    	}
+    	else {
+    		mFinanceItems = DBMgr.getItemsFromCategoryID(mType, mMainCategory.getID());
+    		mTotalAmout = DBMgr.getTotalMainCategoryAmount(mType, mMainCategory.getID());
+    	}
+		
 		updateMapCategory();
 	}
     
-	@Override
+    @Override
 	protected void setTitleBtn() {
-		setTitle("부체분류");
+		if (mMainCategory == null) {
+			setTitle("부체 분류");
+			
+			setTitleBtnText(FmBaseLayout.BTN_LEFT_01, "자산");
+			setTitleBtnVisibility(FmBaseLayout.BTN_LEFT_01, View.VISIBLE);
+		}
+		else {
+			setTitle(mMainCategory.getName() + " 분류");
+		}
+		
 //		setAddButtonListener();
 //		setTitleBtnVisibility(FmTitleLayout.BTN_RIGTH_01, View.VISIBLE);
 //		setTitleBtnText(FmTitleLayout.BTN_RIGTH_01, getResources().getString(R.string.btn_add));
-		
-		setTitleBtnText(FmBaseLayout.BTN_LEFT_01, "자산");
-		setTitleBtnVisibility(FmBaseLayout.BTN_LEFT_01, View.VISIBLE);
 		
 		super.setTitleBtn();
 	}
@@ -69,18 +95,43 @@ public class ReportCompareLiabilityLayout extends ReportBaseCompare {
     }
 	
 	protected void onClickCategoryButton(CategoryAmount categoryAmount) {
-		Intent intent = new Intent(ReportCompareLiabilityLayout.this, ReportLiabilityExpandLayout.class);
-		intent.putExtra(MsgDef.ExtraNames.CATEGORY_ID, categoryAmount.getCategoryID());
-		intent.putExtra(MsgDef.ExtraNames.CATEGORY_NAME, categoryAmount.getName());
-		startActivity(intent);
+//		Intent intent = new Intent(ReportCompareAssetsLayout.this, ReportMonthOfYearCategoryLayout.class);
+//		intent.putExtra(MsgDef.ExtraNames.CATEGORY_ID, categoryAmount.getCategoryID());
+//		intent.putExtra(MsgDef.ExtraNames.ITEM_TYPE, IncomeItem.TYPE);
+//		startActivity(intent);
+		
+		if (mMainCategory == null) {
+			Intent intent = new Intent(ReportCompareLiabilityLayout.this, ReportCompareLiabilityLayout.class);
+			intent.putExtra(MsgDef.ExtraNames.CATEGORY_ID, categoryAmount.getCategoryID());
+			intent.putExtra(MsgDef.ExtraNames.ITEM_TYPE, LiabilityItem.TYPE);
+			startActivity(intent);
+		}
+		else {
+			Intent intent = new Intent(ReportCompareLiabilityLayout.this, ReportChangeAssets.class);
+			intent.putExtra(MsgDef.ExtraNames.ITEM_ID, categoryAmount.getCategoryID());
+			intent.putExtra(MsgDef.ExtraNames.ITEM_TYPE, LiabilityItem.TYPE);
+			startActivity(intent);
+		}
+		
 	}
 	
 	private void setButtonClickListener() {
-	
+		findViewById(R.id.BtnTotalAmount).setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(ReportCompareLiabilityLayout.this, ReportChangeAssets.class);
+				intent.putExtra(MsgDef.ExtraNames.ITEM_TYPE, LiabilityItem.TYPE);
+				if (mMainCategory != null) {
+					intent.putExtra(MsgDef.ExtraNames.CATEGORY_ID, mMainCategory.getID());
+				}
+				
+				startActivity(intent);
+			}
+		});
 	}
 
 	protected void updateChildView() {
-		TextView tvTotalAmount = (TextView)findViewById(R.id.TVTotalAmount);
+		Button tvTotalAmount = (Button)findViewById(R.id.BtnTotalAmount);
 		tvTotalAmount.setText(String.format("금액 : %,d", mTotalAmout));
 		
 		updateBarGraph();
@@ -97,6 +148,26 @@ public class ReportCompareLiabilityLayout extends ReportBaseCompare {
     		}
     	}
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	@Override
+	public void updateMapCategory() {
+		
+		if (mMainCategory == null) {
+			super.updateMapCategory();
+		}
+		else {
+			mCategoryAmount.clear();
+			
+			int itemSize = mFinanceItems.size();
+			
+			for (int index = 0; index < itemSize; index++) {
+				FinanceItem item = mFinanceItems.get(index);
+				CategoryAmount categoryAmount = new CategoryAmount(item.getType());
+				categoryAmount.set(item.getID(), item.getTitle(), item.getAmount());
+				mCategoryAmount.put(item.getID(), categoryAmount);
+			}
+		}
 	}
 
 }
